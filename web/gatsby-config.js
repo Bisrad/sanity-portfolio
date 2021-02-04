56bd5854f1484e6d8513ed851007294b5d54c84d
Brad Thompson <bthompson6225@gmail.com>
2021-02-03 20:57:26 -0700

    Reinitialize Build Envirlonment

---
+++ web/gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteMetadata: {
+    title: `Dev_Bread`,
+    description: `My Development Portfolio`,
+    author: `Brad Thompson`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'epcq1grr',
+        dataset: 'production',
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
