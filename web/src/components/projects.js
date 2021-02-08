982362706fd566ae227188d61ddc5b6b9459dfa8
Brad Thompson <bthompson6225@gmail.com>
2021-02-07 21:43:29 -0700

    Removed Unneeded Content

--- web/src/components/projects.js
+++ web/src/components/projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import Image from 'react-bootstrap/Image'
+import styled from "styled-components";
 // import styled from 'styled-components';
 import img2 from '../images/quips.png';
 import img3 from '../images/mediphor.png';
@@ -9,101 +10,98 @@ import img5 from '../images/BodyBern.png';
 import img6 from '../images/pizza.png';
 
 
-// const LinkStyle = styled.div`
-//   display: block;
-//   margin-top: 1rem;
-//     .icon {
-//       margin-right: 30px;
-//       color: #06BDD6;
-//     }
-// `;
+const ProjectButton = styled.div`
+    display: flex;
+    justify-content: center;
+    border: 1px solid #06BDD6;
+    border-radius: 4px;
+    color:  #06BDD6;
+    max-width: 23vw;
+    padding: 0.25em 1em;
+    margin: 1em 0em;
+        a {
+            font-size:  15px;
+           
+        }
+        :hover {
+            transition: all 0.3s ease 0s;
+            background-color:  #8bedf8;
+            border-color: #8bedf8;
+            color: #181a1f;
+            text-decoration: none;
+        }
+`;
+
+const Title = styled.h2`
+    font-size: 4rem;
+    margin-bottom: 2rem;
+`;
+
+const SubTitle = styled.h3`
+    font-size: 3rem;
+    padding-bottom: 10px;
+    color: #fff;
+    `;
+
 
 class Projects extends React.Component {
     render() {
         return(
             <Container id='projects' style={{ padding: 70 }}>
-            <h1 style={{ marginBottom: '2rem'}}>My Projects</h1>
+            <Title>Recent Projects</Title>
                 <Row style={{ marginBottom: '3rem' }}>
                     <Col>
-                        <Image style={{ borderRadius: '15px' }}src={img2} alt="project img" fluid/>
+                        <Image fluid style={{ borderRadius: '15px' }} src={img2} alt="project img"/>
                     </Col>
-                    <Col style={{ margin: 'auto'}}>
-                        <h1 style={{ paddingBottom: 5,color: '#fff', display: 'flex' }}>| Quips</h1>
-                        <p style={{ fontSize: '15px' }}>
-                        Working on a startup project tackling inconsistencies in the moving industry. 
-                        I helped to update and outdated industry to fit a more modern landscape. 
-                     </p>
-                         <Button 
-                            variant="default"
-                            style={{ width: 350, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
-                            >
+                    <Col>
+                        <SubTitle>| Quips</SubTitle>
+                        <ProjectButton>
                             Visit WebSite
-                        </Button>  
+                        </ProjectButton>  
                     </Col>
                 </Row>
                 <Row style={{ marginBottom: '3rem' }}>
                     <Col>
-                        <Image style={{ borderRadius: '15px' }}src={img6} alt="project img" fluid/>
-
+                        <Image fluid style={{ borderRadius: '15px' }} src={img6} alt="project img"/>
                     </Col>
-                    <Col style={{ margin: 'auto'}}>
-                        <h1 style={{ paddingBottom: 15, color: '#fff', display: 'flex' }}>| Slick's Slices</h1>
-                        <p style={{ fontSize: '15px' }}>A Gatsby static site exploring using the headless CMS sanity and graphQL. This project aims for speed, SEO and an optimal user experience.  </p>
-                         <Button 
-                            variant="default"
-                            style={{ width: 350, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
-                            >
-                            Visit WebSite
-                        </Button>  
+                    <Col>
+                        <SubTitle>| Slick's Slices</SubTitle>
+                        <ProjectButton>
+                            View Deployment
+                        </ProjectButton>  
                     </Col>
                 </Row>
                 <Row style={{ marginBottom: '3rem' }}>
                     <Col>
-                        <Image style={{ borderRadius: '15px' }}src={img3} alt="project img" fluid/>
-
+                        <Image fluid style={{ borderRadius: '15px' }} src={img3} alt="img"/>
                     </Col>
-                    <Col style={{ margin: 'auto'}}>
-                        <h1 style={{ paddingBottom: 15, color: '#fff', display: 'flex' }}>|  Mediphor</h1>
-                        <p style={{ fontSize: '15px' }}>Deisgned for a Industrial Design Firm here in Tempe, AZ. 
-                        This Project aims to showcase company products, goals, and client 
-                        type</p>
-                         <Button 
-                            variant="default"
-                            style={{ width: 350, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
-                            >
-                            Visit WebSite
-                        </Button>  
+                    <Col>
+                        <SubTitle>|  Mediphor</SubTitle>
+                         <ProjectButton>
+                             Visit WebSite
+                         </ProjectButton>  
                     </Col>
                 </Row>
                 <Row style={{ marginBottom: '3rem' }}>
                     <Col>
-                        <Image style={{ borderRadius: '15px' }}src={img4} alt="project img" fluid/>
+                        <Image style={{ borderRadius: '15px' }} src={img4} alt="project img" fluid/>
                     </Col>
-                    <Col style={{ margin: 'auto'}}>
-                        <h1 style={{ paddingBottom: 15, color: '#fff', display: 'flex' }}>|  Hamilton HS</h1>
-                        <p style={{ fontSize: '15px' }}>A Charity site designed for Hamilton High School. This was specialized for a yearly 
-                        event that the school participates in each year</p>
-                         <Button 
-                            variant="default"
-                            style={{ width: 350, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
-                            >
+                    <Col>
+                        <SubTitle>|  Hamilton HS</SubTitle>
+                        <ProjectButton>
                             Visit WebSite
-                        </Button>  
+                        </ProjectButton>  
                     </Col>
                 </Row>
                 <Row style={{ marginBottom: '3rem' }}>
                     <Col>
-                        <Image style={{ borderRadius: '15px' }}src={img5} alt="project img" fluid/>
+                        <Image fluid style={{ borderRadius: '15px' }}src={img5} alt="project img" />
                     </Col>
-                    <Col style={{ margin: 'auto'}}>
-                        <h1 style={{ paddingBottom: 15, color: '#fff', display: 'flex' }}>| BodyBern</h1>
-                        <p style={{ fontSize: '15px' }}>Wordpress build for a Personal Trainer. Currently Under Development</p>
-                         <Button 
-                            variant="default"
-                            style={{ width: 350, borderColor: '#06BDD6', backgroundColor: 'none', color: '#06BDD6'  }}
-                            >
-                            Visit WebSite
-                        </Button>  
+                    <Col>
+                        <SubTitle>| BodyBern</SubTitle>
+                        <ProjectButton>
+                            Currently Underdevelopment
+                        </ProjectButton>  
                     </Col>
                 </Row>
         </Container>    
