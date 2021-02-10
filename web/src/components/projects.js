ef41c9355d3b52838431042a55b90d1c31530ba2
Brad Thompson <bthompson6225@gmail.com>
2021-02-09 21:49:16 -0700

    Updated Project Images

--- web/src/components/projects.js
+++ web/src/components/projects.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Image from 'react-bootstrap/Image'
 import styled from "styled-components";
-// import styled from 'styled-components';
-import img2 from '../images/quips.png';
-import img3 from '../images/mediphor.png';
-import img4 from '../images/Hamilton.png';
-import img5 from '../images/BodyBern.png';
-import img6 from '../images/pizza.png';
 
+import img1 from '../images/Quips-Grid-Image.png';
+import img2 from '../images/Mediphor-Grid-Image.png';
+import img3 from '../images/Pizza-Grid-Image.png';
+import img4 from '../images/Hoi4_Grid-Image.png';
+import img5 from '../images/BB-Grid-Image.png';
+
+import Button from "../theme/Button"
 
 const ProjectButton = styled.div`
     display: flex;
@@ -48,45 +49,32 @@ class Projects extends React.Component {
               {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
   <Row>
     <Col sm={8} md style={{ marginBottom: '2rem', margin: 'auto' }} >
-    <Image fluid  rounded src={img2} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Image fluid  rounded src={img1} alt="Quips"/>
+    <Button primary as="a" href="https://www.quips.com/">Visit Site</Button>
     </Col>
     <Col sm={8} md style={{ marginBottom: '2rem', margin: 'auto' }}>
-    <Image fluid rounded src={img3} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Image fluid rounded src={img2} alt="Mediphor"/>
+    <Button primary as="a" href="https://www.mediphor.com/">Visit Site</Button>
     </Col>
     <Col sm={8} md style={{ marginBottom: '2rem', margin: 'auto' }}>
-    <Image fluid rounded  src={img4} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Image fluid rounded  src={img3} alt="Pizza"/>
+    <Button primary as="a" href="https://gatsby.pizza/slicemaster/slick">Visit Site</Button>
     </Col>
   </Row>
   <Row>
     <Col sm={8} md style={{ marginTop: '1.8rem', margin: 'auto' }} >
-    <Image fluid  rounded src={img5} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Image fluid  rounded src={img4} alt="Hamilton"/>
+    <Button primary as="a" href="http://hamiltononeinfour.com/">Visit Site</Button>
     </Col>
     <Col sm={8} md style={{ marginBottom: '2rem', margin: 'auto' }}>
-    <Image fluid rounded src={img6} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Image fluid rounded src={img5} alt="BodyBern"/>
+    <Button primary as="a" href="https://bodybern.com/">UnderDevelopment</Button>
     </Col>
     <Col sm={8} md style={{ marginTop: '2.8rem', margin: 'auto' }}>
     <Image fluid rounded  src={img4} alt="Quips"/>
-    <ProjectButton>
-        Visit WebSite
-    </ProjectButton>  
+    <Button primary as="a" href="http://hamiltononeinfour.com/">Visit Site</Button>
     </Col>
-  </Row>
-               
+  </Row>        
         </Container>    
         )
     }
